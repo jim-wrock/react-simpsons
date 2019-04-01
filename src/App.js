bbb6b830ff16bc8758c40290f84f4b8f9dd53040
Jim Decocq <jim.decocq@gmail.com>
2019-04-01 14:08:30 +0200

    exercise done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./working.gif";
+import logo2 from "./notworking.gif";
+import "./App.css";
+
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
+// import Quote from "./Quote";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Simpsons Quotes</h1>
+//         </header>
+//         <Quote
+//           quote="I believe the children are the future... Unless we stop them now!"
+//           character="Homer Simpson"
+//           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+//         />
+//         <Quote
+//           quote="Me fail English? That's unpossible"
+//           character="Ralph Wiggum"
+//           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+//         />
+//       </div>
+//     );
+//   }
+// }
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: false
+    };
+  }
+  handleClick = () => {
+    console.log('Button clicked');
+    this.setState({ working: !this.state.working });
+  };
+
   render() {
+    const isWorking = this.state.working ? 'on' : 'off';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <React.Fragment>
+        <div className="Lamp">
+
+          <button
+            onClick={this.handleClick}
+            // onClick={() => alert('Button clicked')}
+            className={isWorking}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            {isWorking.toUpperCase()}
+          </button>
+
+          {/* <button className={light}>{light.toUpperCase()}</button> */}
+          <figure className={isWorking} />
+        </div>
+
+
+
+        <div className="App">
+
+          <header className="App-header">
+            <img src={this.state.working ? logo : logo2} className="App-logo" alt="logo" />
+
+
+          </header>
+
+          <Quotes />
+          <Lamp on />
+          <Lamp />
+        </div>
+      </React.Fragment>
+
     );
   }
 }
+
+
+
 
 export default App;
